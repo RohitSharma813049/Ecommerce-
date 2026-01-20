10595a336e2cc6212b3098a036baf6beb2038e36
Rohit <rohitsharma813049@gmail.com>
2026-01-20 20:58:15 +0530

    Fix import path for CartContextupdate

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { WishlistProvider } from './user/context/WishlistContext'
-import { CartProvider } from './user/context/Cartcontext'
+import { CartProvider } from './user/context/CartContext'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
